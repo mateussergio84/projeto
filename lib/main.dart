d9bc7acb0147bab1ca5528c5ad230d46afc50604
mateus <mateussergio84>
2021-02-24 22:09:04 -0300

    Lista+ detalhes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/lista.dart';
 import 'package:flutter_application_1/login.dart';
 import 'cadUsuario.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: login(),
+    home: lista(),
   ));
 }
